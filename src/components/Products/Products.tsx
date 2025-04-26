a4d2584e7785231e4b2c63782f8b91d6db72c6b5
Enzo Osorio <enzoosorio2010@hotmail.com>
2025-04-26 14:53:12 -0500

    avance en products minimo
    Avance para poder crear la seccion general de productos

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SectionBlank } from '../reusable/SectionBlank'
+
+export const Products = () => {
+  return (
+    <SectionBlank>
+       <p></p> 
+    </SectionBlank>
+  )
+}
